c0ea9020bfa4b0e67c886f7f169711e7898588c5
Albert Vilella <avilella@gmail.com>
2023-06-19 09:18:58 +0100

    from inputfile list to output file csv

--- run_space2.py
+++ run_space2.py
@@ -1,21 +1,47 @@
+import sys
+import os.path
 import argparse
 import glob
 import pandas as pd
+from pathlib import Path
 import SPACE2
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser(description='Run SPACE2 clustering on .pdb files in a folder.')
-parser.add_argument('-d', '--folder', help='Folder path containing .pdb files', required=True)
+parser.add_argument('-i', '--inputfile', help='File containing the full path location of all the .pdb files', required=True)
+parser.add_argument('-t', '--tag', help='File containing the full path location of all the .pdb files', required=False, default='spa2')
+parser.add_argument("-r", "--refresh",     help="refresh", action="store_true")
+parser.add_argument("-d", "--debug",     help="debug", action="store_true")
+parser.add_argument("-v", "--verbose",     help="verbose", action="store_true")
+
 args = parser.parse_args()
 
-# Get the folder path from command-line argument
-folder_path = args.folder
+# Start program
+sys.stderr.write("# inputfile is %s\n" % args.inputfile)
+
+(dirname,filename) = os.path.split(args.inputfile)
+(filebasename,file_extension) = os.path.splitext(filename)
+
+outfile = dirname + '/' + filebasename + '.' + args.tag + '.csv'
+if not args.refresh:
+    p = Path(outfile)
+    if (p.exists()) and (os.stat(outfile).st_size>180):
+        print(outfile)
+        sys.exit(0)
 
 # Get the list of .pdb files in the specified folder
-antibody_models = glob.glob(folder_path + '/*.pdb')
+# Read the file paths from input.txt
+with open(args.inputfile, 'r') as file:
+    sys.stderr.write("# reading %s\n" % args.inputfile)
+    file_paths = file.read().splitlines()
 
 # Run agglomerative clustering
-df = SPACE2.agglomerative_clustering(antibody_models, cutoff=1.25)
+df = SPACE2.agglomerative_clustering(file_paths, cutoff=1.25)
+
+
+#antibody_models = glob.glob(folder_path + '/*.pdb')
+## Run agglomerative clustering
+#df = SPACE2.agglomerative_clustering(antibody_models, cutoff=1.25)
 
 # Print the DataFrame to a CSV file
-df.to_csv(folder_path + 'output.csv', index=False)
+df.to_csv(outfile, index=False)
